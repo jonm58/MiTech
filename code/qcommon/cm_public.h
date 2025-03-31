92d5cd63709349e68672bb05fc8909c171bd6fc2
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2025-03-31 20:54:35 +0300

    Update cm_public.h

--- code/qcommon/cm_public.h
+++ code/qcommon/cm_public.h
@@ -52,6 +52,10 @@ void		CM_TransformedBoxTrace( trace_t *results, const vec3_t start, const vec3_t
 						const vec3_t mins, const vec3_t maxs,
 						clipHandle_t model, int brushmask,
 						const vec3_t origin, const vec3_t angles, qboolean capsule );
+void		CM_TransformedBoxTrace_SourceTech( trace_t *results, const vec3_t start, const vec3_t end,
+						const vec3_t mins, const vec3_t maxs,
+						clipHandle_t model, int brushmask,
+						const vec3_t origin, const vec3_t angles, qboolean capsule );
 
 byte		*CM_ClusterPVS (int cluster);
 
