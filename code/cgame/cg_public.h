17c585277d1c1f1741df0652177f2acd91971e76
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2025-11-03 17:04:00 +1200

    Big cleanup

--- code/cgame/cg_public.h
+++ code/cgame/cg_public.h
@@ -1,24 +1,6 @@
-/*
-===========================================================================
-Copyright (C) 1999-2005 Id Software, Inc.
-
-This file is part of Quake III Arena source code.
-
-Quake III Arena source code is free software; you can redistribute it
-and/or modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 2 of the License,
-or (at your option) any later version.
-
-Quake III Arena source code is distributed in the hope that it will be
-useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Quake III Arena source code; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-===========================================================================
-*/
+// Copyright (C) 1999-2005 ID Software, Inc.
+// Copyright (C) 2023-2025 Noire.dev
+// SourceTech — GPLv2; see LICENSE for details.
 
 #define	CMD_BACKUP			64	
 #define	CMD_MASK			(CMD_BACKUP - 1)
@@ -37,23 +19,8 @@ typedef struct {
 } snapshot_t;
 
 typedef enum {
-	CG_PRINT,
-	CG_ERROR,
-	CG_MILLISECONDS,
-	CG_CVAR_REGISTER,
-	CG_CVAR_UPDATE,
-	CG_CVAR_SET,
-	CG_ARGC,
-	CG_ARGV,
-	CG_ARGS,
-	CG_FS_FOPENFILE,
-	CG_FS_READ,
-	CG_FS_WRITE,
-	CG_FS_FCLOSEFILE,
-	CG_SENDCONSOLECOMMAND,
 	CG_ADDCOMMAND,
 	CG_SENDCLIENTCOMMAND,
-	CG_UPDATESCREEN,
 	CG_CM_LOADMAP,
 	CG_CM_NUMINLINEMODELS,
 	CG_CM_INLINEMODEL,
@@ -64,27 +31,13 @@ typedef enum {
 	CG_CM_TRANSFORMEDBOXTRACE,
 	CG_CM_MARKFRAGMENTS,
 	CG_S_STARTSOUND,
-	CG_S_STARTLOCALSOUND,
 	CG_S_CLEARLOOPINGSOUNDS,
 	CG_S_ADDLOOPINGSOUND,
 	CG_S_UPDATEENTITYPOSITION,
 	CG_S_RESPATIALIZE,
-	CG_S_REGISTERSOUND,
 	CG_S_STARTBACKGROUNDTRACK,
 	CG_R_LOADWORLDMAP,
-	CG_R_REGISTERMODEL,
-	CG_R_REGISTERSKIN,
-	CG_R_REGISTERSHADER,
-	CG_R_CLEARSCENE,
-	CG_R_ADDREFENTITYTOSCENE,
-	CG_R_ADDPOLYTOSCENE,
-	CG_R_ADDLIGHTTOSCENE,
-	CG_R_RENDERSCENE,
-	CG_R_SETCOLOR,
-	CG_R_DRAWSTRETCHPIC,
 	CG_R_MODELBOUNDS,
-	CG_R_LERPTAG,
-	CG_GETGLCONFIG,
 	CG_GETGAMESTATE,
 	CG_GETCURRENTSNAPSHOTNUMBER,
 	CG_GETSNAPSHOT,
@@ -92,19 +45,10 @@ typedef enum {
 	CG_GETCURRENTCMDNUMBER,
 	CG_GETUSERCMD,
 	CG_SETUSERCMDVALUE,
-	CG_R_REGISTERSHADERNOMIP,
-	CG_KEY_GETCATCHER,
 	CG_KEY_GETKEY,
-	CG_REAL_TIME,
-	CG_R_REMAP_SHADER,
 	CG_S_ADDREALLOOPINGSOUND,
 	CG_S_STOPLOOPINGSOUND,
-	CG_SYSTEM,
-	CG_R_ADDLINEARLIGHTTOSCENE,
-	CG_FS_GETFILELIST,
-	CG_IMPORTOBJ,
-
-	CG_ACOS = 1007
+	CG_IMPORTOBJ
 } cgameImport_t;
 
 typedef enum {
