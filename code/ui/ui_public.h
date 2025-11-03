33a49450ec96c26597c02a7d3702a9f17b3d01a2
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2025-11-03 17:05:42 +1200

    Big cleanup

--- code/ui/ui_public.h
+++ code/ui/ui_public.h
@@ -1,27 +1,6 @@
-/*
-===========================================================================
-Copyright (C) 1999-2005 Id Software, Inc.
-
-This file is part of Quake III Arena source code.
-
-Quake III Arena source code is free software; you can redistribute it
-and/or modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 2 of the License,
-or (at your option) any later version.
-
-Quake III Arena source code is distributed in the hope that it will be
-useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Quake III Arena source code; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-===========================================================================
-*/
-
-#ifndef __UI_PUBLIC_H__
-#define __UI_PUBLIC_H__
+// Copyright (C) 1999-2005 ID Software, Inc.
+// Copyright (C) 2023-2025 Noire.dev
+// SourceTech — GPLv2; see LICENSE for details.
 
 typedef struct {
 	connstate_t		connState;
@@ -33,60 +12,24 @@ typedef struct {
 } uiClientState_t;
 
 typedef enum {
-	UI_ERROR,
-	UI_PRINT,
-	UI_MILLISECONDS,
-	UI_CVAR_SET,
-	UI_CVAR_SETVALUE,
-	UI_CVAR_RESET,
-	UI_ARGC,
-	UI_ARGV,
-	UI_CMD_EXECUTETEXT,
-	UI_FS_FOPENFILE,
-	UI_FS_READ,
-	UI_FS_WRITE,
-	UI_FS_FCLOSEFILE,
-	UI_FS_GETFILELIST,
-	UI_R_REGISTERMODEL,
-	UI_R_REGISTERSKIN,
-	UI_R_REGISTERSHADERNOMIP,
-	UI_R_CLEARSCENE,
-	UI_R_ADDREFENTITYTOSCENE,
-	UI_R_ADDPOLYTOSCENE,
-	UI_R_ADDLIGHTTOSCENE,
-	UI_R_RENDERSCENE,
-	UI_R_SETCOLOR,
-	UI_R_DRAWSTRETCHPIC,
-	UI_UPDATESCREEN,
-	UI_CM_LERPTAG,
-	UI_S_REGISTERSOUND,
-	UI_S_STARTLOCALSOUND,
-	UI_KEY_KEYNUMTOSTRINGBUF,
-	UI_KEY_GETBINDINGBUF,
-	UI_KEY_SETBINDING,
-	UI_KEY_ISDOWN,
-	UI_KEY_GETOVERSTRIKEMODE,
-	UI_KEY_SETOVERSTRIKEMODE,
-	UI_KEY_CLEARSTATES,
-	UI_KEY_GETCATCHER,
-	UI_KEY_SETCATCHER,
-	UI_GETCLIPBOARDDATA,
-	UI_GETGLCONFIG,
-	UI_GETCLIENTSTATE,
-	UI_GETCONFIGSTRING,
-	UI_LAN_GETPINGQUEUECOUNT,
-	UI_LAN_CLEARPING,
-	UI_LAN_GETPING,
-	UI_LAN_GETPINGINFO,
-	UI_CVAR_REGISTER,
-	UI_CVAR_UPDATE,
-	UI_MEMORY_REMAINING,
-	UI_REAL_TIME,
-	UI_LAN_GETSERVERCOUNT,
-	UI_LAN_GETSERVERADDRESSSTRING,
-	UI_R_REMAP_SHADER,
-	UI_SYSTEM,
-	UI_R_ADDLINEARLIGHTTOSCENE
+    UI_KEY_KEYNUMTOSTRINGBUF,
+    UI_KEY_GETBINDINGBUF,
+    UI_KEY_SETBINDING,
+    UI_KEY_ISDOWN,
+    UI_KEY_GETOVERSTRIKEMODE,
+    UI_KEY_SETOVERSTRIKEMODE,
+    UI_KEY_CLEARSTATES,
+    UI_KEY_SETCATCHER,
+    UI_GETCLIPBOARDDATA,
+    UI_GETCLIENTSTATE,
+    UI_GETCONFIGSTRING,
+    UI_LAN_GETPINGQUEUECOUNT,
+    UI_LAN_CLEARPING,
+    UI_LAN_GETPING,
+    UI_LAN_GETPINGINFO,
+    UI_MEMORY_REMAINING,
+    UI_LAN_GETSERVERCOUNT,
+    UI_LAN_GETSERVERADDRESSSTRING
 } uiImport_t;
 
 typedef enum {
@@ -106,5 +49,3 @@ typedef enum {
 	UI_CONSOLE_COMMAND,
 	UI_DRAW_CONNECT_SCREEN
 } uiExport_t;
-
-#endif
