712b888b0bb58a754c26dde5e13d8148de6663e1
noire-dev <141606820+noire-dev@users.noreply.github.com>
2025-11-09 11:36:06 +1200

    Update: 2025-11-09 11:36

--- code/server/sv_game.c
+++ code/server/sv_game.c
@@ -29,7 +29,7 @@ playerState_t* SV_GameClientNum(int num) {
 }
 
 svEntity_t* SV_SvEntityForGentity(sharedEntity_t* gEnt) {
-	if(!gEnt || gEnt->s.number < 0 || gEnt->s.number >= MAX_GENTITIES) Com_Error(ERR_DROP, "SV_SvEntityForGentity: bad gEnt");
+	if(!gEnt || gEnt->s.number < 0 || gEnt->s.number >= MAX_GENTITIES) return NULL; //Com_Error(ERR_DROP, "SV_SvEntityForGentity: bad gEnt");
 
 	return &sv.svEntities[gEnt->s.number];
 }
